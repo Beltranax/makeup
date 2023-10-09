98f68c2d09321483e5a5c2112b8a256673c962f2
Angel Beltran <beltranangel7293@gmail.com>
2023-10-09 13:03:26 -0700

    second commit aws - resizing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -195,7 +195,7 @@ const NavBar = () => {
 
                 <button
                     onClick={() => setMode(mode === "light" ? "dark" : "light")}
-                    className={`ml-3 flex items-center justify-center rounded-full p-1
+                    className={`ml-3 sm:mx-1 flex items-center justify-center rounded-full p-1
                     ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}
                     `}
                 >
